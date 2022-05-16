27bff0079b631d7adb4802ac3c04885b81df2c25
Aaron.Zhang <bozhang@ebay.com>
2022-05-16 12:34:53 +0800

    Add remote address into debug log for TLS error (#21280)
    Signed-off-by: bozhang <bozhang@ebay.com>

--- source/extensions/transport_sockets/tls/ssl_socket.cc
+++ source/extensions/transport_sockets/tls/ssl_socket.cc
@@ -233,12 +233,9 @@ void SslSocket::drainErrorQueue(const bool show_errno) {
                                         absl::NullSafeStringView(ERR_reason_error_string(err))));
   }
   if (!failure_reason_.empty()) {
-    if (show_errno) {
-      ENVOY_CONN_LOG(debug, "errno:{}:{}:{}", callbacks_->connection(), errno,
-                     Envoy::errorDetails(errno), failure_reason_);
-    } else {
-      ENVOY_CONN_LOG(debug, "{}", callbacks_->connection(), failure_reason_);
-    }
+    ENVOY_CONN_LOG(debug, "remote address:{},{}", callbacks_->connection(),
+                   callbacks_->connection().connectionInfoProvider().remoteAddress()->asString(),
+                   failure_reason_);
   }
   if (saw_error && !saw_counted_error) {
     ctx_->stats().connection_error_.inc();
