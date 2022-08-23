68c4c98ec6678c0f3ce95cedfb195b1b0988902d
Paul Sohn <paulsohn@google.com>
2022-08-23 12:57:17 -0400

    stream_info: add downstream handshake completion timing (#22367)
    Signed-off-by: Paul Sohn <paulsohn@google.com>

--- source/extensions/transport_sockets/tls/ssl_socket.cc
+++ source/extensions/transport_sockets/tls/ssl_socket.cc
@@ -195,6 +195,9 @@ void SslSocket::onSuccess(SSL* ssl) {
         ->upstreamTiming()
      
         .onUpstreamHandshakeComplete(callbacks_->connection().dispatcher().timeSource());
+  } else {
+    callbacks_->connection().streamInfo().downstreamTiming().onDownstreamHandshakeComplete(
+        callbacks_->connection().dispatcher().timeSource());
   }
 
   callbacks_->raiseEvent(Network::ConnectionEvent::Connected);
