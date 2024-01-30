3181c7024c367b621549cd373b28dd18009ed52b
Jonh Wendell <jwendell@redhat.com>
2024-01-30 10:14:31 -0500

    Initial adjustements
    Build is enabled in CI but tests are not.

--- maistra/run-ci.sh
+++ maistra/run-ci.sh
@@ -11,23 +11,22 @@ export BUILD_SCM_REVISION="Maistra PR #${PULL_NUMBER:-undefined}"
 export BUILD_SCM_STATUS="SHA=${PULL_PULL_SHA:-undefined}"
 
 # Build
-time bazel --output_base=/bazel-cache/BASE build \
-  --disk_cache=/bazel-cache \
-  --override_repository=com_github_google_jwt_verify=/work/jwt_verify_lib \
+time bazel build \
   ${COMMON_FLAGS} \
   //source/exe:envoy-static
 
 echo "Build succeeded. Binary generated:"
 bazel-bin/source/exe/envoy-static --version
 
+# FIXME: Do not run tests
+exit 0
+
 # By default, `bazel test` command performs simultaneous
 # build and test activity.
 # The following build step helps reduce resources usage
 # by compiling tests first.
 # Build tests
-time bazel --output_base=/bazel-cache/BASE build \
-  --disk_cache=/bazel-cache \
-  --override_repository=com_github_google_jwt_verify=/work/jwt_verify_lib \
+time bazel build \
   ${COMMON_FLAGS} \
   --build_tests_only \
   -- \
@@ -35,8 +34,7 @@ time bazel --output_base=/bazel-cache/BASE build \
   # -//test/server:listener_manager_impl_quic_only_test
 
 # Run tests
-time bazel --output_base=/bazel-cache/BASE test \
-  --disk_cache=/bazel-cache \
+time bazel test \
   ${COMMON_FLAGS} \
   --build_tests_only \
   -- \
